d39cb02b9f6a9facb07a98f0b65860cccbf1f41a
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2019-05-29 09:43:15 +0530

    first commit

---
+++ ConferenceScheduler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConferenceScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
